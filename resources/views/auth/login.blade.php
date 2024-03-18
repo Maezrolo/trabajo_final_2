8f1602e9aef23653ebe404551b847ece9e2c2a01
Maezrolo <marcosuse007@gmail.com>
2024-03-18 16:47:00 -0300

    log_terminado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="es-Ar">
 
 <head>
-    <title>Title</title>
+    <title>Iniciar Seccion</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -12,8 +12,13 @@
 </head>
 
 <style>
-    body{
+    body {
         background: #eae0d4;
+    }
+
+    form {
+        background:#e5e5e5;
+        border: 2px solid black;
     }
 </style>
 
@@ -25,10 +30,10 @@
                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image">
                 </div>
                 <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-                    
-                <!-- inicio del formulario -->
-                    <form class="bg-light bg-info p-4"">
-                    <h2 class="fw-bold mb-2 text-uppercase text-center text-primary">Iniciar Sesión</h2>
+
+                    <!-- inicio del formulario -->
+                    <form class="p-4">
+                        <h2 class="fw-bold mb-2 text-uppercase text-center text-primary">Iniciar Sesión</h2>
 
                         <!-- Email input -->
                         <div class="form-outline mb-4">
@@ -55,18 +60,18 @@
                         <button type="submit" class="btn btn-primary btn-lg btn-block">Iniciar Sesión</button>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm  text-gray-700 underline">Registro</a>
+                        <a href="{{ route('register') }}" class="ml-4 text-sm  text-gray-700 underline">Registro</a>
                         @endif
 
-                        <div class="divider d-flex align-items-center my-4">
+                        <!-- <div class="divider d-flex align-items-center my-4">
                             <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
                         </div>
 
                         <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="#!" role="button">
-                            <i class="fab fa-facebook-f me-2"></i>Continue with Facebook
+                        <i class="bi bi-facebook"></i>Continue with Facebook
                         </a>
                         <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!" role="button">
-                            <i class="fab fa-twitter me-2"></i>Continue with Twitter</a>
+                        <i class="fab fa-twitter me-2"></i>Continue with Twitter</a> -->
 
                     </form>
                 </div>
